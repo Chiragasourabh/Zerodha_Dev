a65b0b12b8f0b4ee8f720a979299aacdf726ace1
chiragasourabh <chiragasourabh@gmail.com>
2019-08-10 13:29:45 +0530

    Search

--- CherryPy App/app.py
+++ CherryPy App/app.py
@@ -17,15 +17,23 @@ class Root:
         return """<a href="MarketInfo"><h1>Go to MarketInfo!</h1></a>"""
 
     @cherrypy.expose
-    def MarketInfo(self, length=8):
+    def MarketInfo(self, scname = None):
         tmpl = env.get_template('MarketInfo.html')
         today = datetime.date.today().strftime("%d-%m-%Y")
-        topTenCode = redisConn.lrange('top10Keys', 0, 10)
-        topTenCode.reverse()
         answerList = []
-        for code in topTenCode:
-            answerList.append(redisConn.hmget(code.decode(), "SC_NAME", "OPEN", "HIGH", "LOW", "CLOSE"))
-        return tmpl.render(date =  today, code = topTenCode, data = answerList)
+        scCodes = []
+        if scname:
+            matchingNames = redisConn.scan_iter("*"+scname.upper()+"*")
+            for name in matchingNames:
+                scCode = redisConn.mget(name.decode())[0]
+                scCodes.append(scCode)
+                answerList.append(redisConn.hmget(scCode.decode(), "SC_NAME", "OPEN", "HIGH", "LOW", "CLOSE"))
+        else:
+            scCodes = redisConn.lrange('top10Keys', 0, 10)
+            for code in scCodes:
+                answerList.append(redisConn.hmget(code.decode(), "SC_NAME", "OPEN", "HIGH", "LOW", "CLOSE"))
+            
+        return tmpl.render(date =  today, code = scCodes, data = answerList)
 
 conf=   {
         "/css": {
